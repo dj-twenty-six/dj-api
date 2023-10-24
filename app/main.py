e22dd1afba1b36378ea3b4a2ec52ff711675a292
pysatellite <pysatellite@gmail.com>
2023-10-24 14:05:05 +0900

    add api dj weather

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import Union
-
 from fastapi import FastAPI
+from dj_weather_show.weather_show import get_weather
 
 app = FastAPI()
 
@@ -13,3 +13,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/weather")
+def read_root():
+    """DJ weather"""
+    location, weather_condition, temperature = get_weather()
+    return {"location": location, "weather_condition": weather_condition, "temperature": temperature}

d9d74b7df6a4096c1e1eec5b3ac6ab9b74e5273d
dmario24 <becky2sawyer@gmail.com>
2023-11-03 21:50:01 +0900

    vs RUST iam best
    - [ ] vs RUST - https://github.com/pySatellite/hello-actix/pull/2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from dj_weather_show.weather_show import get_weather
+import time
 
 app = FastAPI()
 
@@ -14,8 +15,26 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.get("/weather")
 def read_root():
     """DJ weather"""
     location, weather_condition, temperature = get_weather()
     return {"location": location, "weather_condition": weather_condition, "temperature": temperature}
+
+
+@app.get("/iambest/{limit:int}")
+async def iambest(limit: int):
+    start = time.time()
+    sum: int = 0
+    for i in range(limit):
+        sum += i
+
+    duration = time.time() - start
+
+    if sum > 2**63 - 1:
+        raise HTTPException(
+            status_code=400, detail="Sum overflow."
+        )
+
+    return {"duration": duration, "limit": limit, "sum": sum}
